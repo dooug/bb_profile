8561f96f87ce262a8c6387e2cdb0dae0f304a3c3
Benjamin Chodoroff <ben@theworkdept.com>
2014-04-12 02:14:40 +0000

    patch submitted in https://drupal.org/node/2106687

--- modules/commerce_kickstart/commerce_kickstart_search/commerce_kickstart_search.js
+++ modules/commerce_kickstart/commerce_kickstart_search/commerce_kickstart_search.js
@@ -1,5 +1,14 @@
 (function ($) {
 
+var oldLive = $.fn.live;
+$.fn.live = function( types, data, fn ) {
+  if ( oldLive ) {
+    return oldLive.apply( this, arguments );
+  }
+  $( this.context ).on( types, this.selector, data, fn );
+  return this;
+};
+
   // Disable checkboxes when the user clicks on one of them. (prevent
   // multi-clic
   Drupal.behaviors.kickstartSearch = {
